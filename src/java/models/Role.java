34f2582f1daf1dfe7933cf70145eb4972d1f404a
emily <emily@Emilys-Book>
2021-07-10 02:05:02 -0600

    Second Commit created the persistence and re-created the models folder

--- src/java/models/Role.java
+++ src/java/models/Role.java
@@ -1,9 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package models;
 
-public class Role {
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 
+/**
+ *
+ * @author emily
+ */
+@Entity
+@Table(name = "role")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Role.findAll", query = "SELECT r FROM Role r")
+    , @NamedQuery(name = "Role.findByRoleId", query = "SELECT r FROM Role r WHERE r.roleId = :roleId")
+    , @NamedQuery(name = "Role.findByRoleName", query = "SELECT r FROM Role r WHERE r.roleName = :roleName")})
+public class Role implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "role_id")
     private int roleID;
+    @Basic(optional = false)
+    @Column(name = "role_name")
     private String roleName;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "role", fetch = FetchType.EAGER)
+    private List<User> userList;
 
     public Role() {
     }
@@ -13,7 +52,7 @@ public class Role {
         this.roleName = roleName;
     }
 
-    public int getRoleID() {
+      public int getRoleID() {
         return roleID;
     }
 
@@ -29,4 +68,13 @@ public class Role {
         this.roleName = roleName;
     }
 
+    @XmlTransient
+    public List<User> getUserList() {
+        return userList;
+    }
+
+    public void setUserList(List<User> userList) {
+        this.userList = userList;
+    }
+    
 }
